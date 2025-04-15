2109712cb87e20fc97a48eb47e4f9933b1e4ecce
xuvah <augusto.filipe838@gmail.com>
2025-04-14 21:50:28 -0300

    eficiency upgrades in BFS only

--- 15game.py
+++ 15game.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import random
 import tkinter as tk
-import copy
 import time
 from collections import deque
 
@@ -30,7 +29,6 @@ class GameGUI:
                 self.buttons[i][j]['text'] = text
 
     def handle_click(self, i, j):
-        # tenta mover se for adjacente ao espaço vazio
         zero_i = self.game.zero_pos['i']
         zero_j = self.game.zero_pos['j']
         if abs(zero_i - i) + abs(zero_j - j) == 1:
@@ -41,18 +39,23 @@ class GameGUI:
 class Game15:
     def __init__(self, table=None):
         if table:
-            self.table = table
+            self.table = [row[:] for row in table]  # cópia profunda leve
         else:
             self.table = self._generate_random_table()
         self.zero_pos = self._find_zero()
         self.move = ""
 
-# método de criação
+    @classmethod
+    def from_parent(cls, table, zero_pos, move):
+        obj = cls(table)
+        obj.zero_pos = zero_pos
+        obj.move = move
+        return obj
+
     def _generate_random_table(self):
         flat = list(range(16))
         random.shuffle(flat)
-        table = [flat[i * 4:(i + 1) * 4] for i in range(4)]
-        return table
+        return [flat[i * 4:(i + 1) * 4] for i in range(4)]
 
     def __eq__(self, other):
         return isinstance(other, Game15) and self.table == other.table
@@ -60,6 +63,9 @@ class Game15:
     def __hash__(self):
         flat = tuple(num for row in self.table for num in row)
         return hash(flat)
+
+    def get_state_id(self):
+        return tuple(num for row in self.table for num in row)
 
     def _find_zero(self):
         for i in range(4):
@@ -69,12 +75,8 @@ class Game15:
         return None
 
     def get_moves(self, table_zero=None, last_move=None):
-        if table_zero:
-            i = table_zero['i']
-            j = table_zero['j']
-        else:
-            i = self.zero_pos['i']
-            j = self.zero_pos['j']
+        i = table_zero['i'] if table_zero else self.zero_pos['i']
+        j = table_zero['j'] if table_zero else self.zero_pos['j']
 
         possible_moves = {
             "Up": i > 0,
@@ -83,7 +85,6 @@ class Game15:
             "Right": j < 3
         }
 
-        # Remover o movimento reverso (não desfazer o último passo)
         reverse = {
             "Up": "Down",
             "Down": "Up",
@@ -96,97 +97,88 @@ class Game15:
 
         return possible_moves
 
-    
     def test_factible(self):
         inversions = 0
-        flat = [num if num != 0 else "_" for row in self.table for num in row]
+        flat = [num for row in self.table for num in row if num != 0]
         zero_row = 4 - self.zero_pos['i']
-        for i in range(len(flat)-1):
-            try:
-                if flat[i] > flat[i+1]:
+        for i in range(len(flat)):
+            for j in range(i + 1, len(flat)):
+                if flat[i] > flat[j]:
                     inversions += 1
-            except:
-                pass #tratativa para não realizar comparações entre o vazio (0) e os números
         return (inversions + zero_row) % 2 != 0
 
-    def print_board(self,state=None):
-        
-        if state:
-            print("\033c", end="")
-            for row in state.table:
-                print(" ".join(f"{n if n != 0 else '_':>2}" for n in row))
-        else:
-            print("\033c", end="")
-            for row in self.table:
-                print(" ".join(f"{n if n != 0 else '_':>2}" for n in row))
+    def print_board(self):
+        print("\033c", end="")
+        for row in self.table:
+            print(" ".join(f"{n if n != 0 else '_':>2}" for n in row))
 
     def do_moves(self):
         i, j = self.zero_pos['i'], self.zero_pos['j']
         new_states = []
 
         move_offsets = {
-            "Up":    (-1, 0),
-            "Down":  (1, 0),
-            "Left":  (0, -1),
+            "Up": (-1, 0),
+            "Down": (1, 0),
+            "Left": (0, -1),
             "Right": (0, 1)
         }
 
-        for move, allowed in self.get_moves().items():
+        for move, allowed in self.get_moves(self.zero_pos, self.move).items():
             if allowed:
                 di, dj = move_offsets[move]
                 ni, nj = i + di, j + dj
-                new_table = [row[:] for row in self.table]  # cópia rasa
+                new_table = [row[:] for row in self.table]
                 new_table[i][j], new_table[ni][nj] = new_table[ni][nj], new_table[i][j]
-                new_state = Game15(new_table)
-                new_state.zero_pos = {'i': ni, 'j': nj}
-                new_state.move = move
+                new_state = Game15.from_parent(new_table, {'i': ni, 'j': nj}, move)
                 new_states.append(new_state)
 
         return new_states
 
-    def solve_game(self, meta=None, method='BFS'):
+    def solve_game(self, meta=None):
         if not meta:
             meta_flat = list(range(1, 16)) + [0]
             meta = [meta_flat[i * 4:(i + 1) * 4] for i in range(4)]
 
-        meta_game = Game15(meta)
+        meta_id = tuple(num for row in meta for num in row)
         queue = deque()
         visited = set()
 
-        initial_state = copy.deepcopy(self)
+        initial_state = Game15.from_parent(self.table, self.zero_pos.copy(), "")
         queue.append((initial_state, []))
-        visited.add(initial_state)
+        visited.add(initial_state.get_state_id())
 
         while queue:
             current_state, path = queue.popleft()
 
-            if current_state == meta_game:
+            if current_state.get_state_id() == meta_id:
+                print(f"Solução encontrada com {len(path)} movimentos")
                 return path
 
             next_states = current_state.do_moves()
 
-
             for next_state in next_states:
-                if next_state not in visited:
+                next_id = next_state.get_state_id()
+                if next_id not in visited:
                     queue.append((next_state, path + [next_state.move]))
-                    visited.add(next_state)
+                    visited.add(next_id)
 
+            if len(visited) % 100000 == 0:
+                print(f"Visitados: {len(visited)} - Profundidade: {len(path)}")
 
-#main
+# main
 if __name__ == "__main__":
     easy = [
         [1, 2, 4, 3],
         [5, 6, 8, 7],
-        [14, 13, 15, 12],
+        [15, 13, 14, 12],
         [11, 10, 0, 9]
     ]
     game = Game15(easy)
     game.print_board()
-    movimentos= game.get_moves()
-    print ( "jogo solucioavel?", game.test_factible())
+    print("Solucionável?", game.test_factible())
     time.sleep(1)
     solve_path = game.solve_game()
-    print("Solução: ", solve_path)
+    print("Solução:", solve_path)
     root = tk.Tk()
     root.title("Jogo dos 15")
     gui = GameGUI(root, game)
